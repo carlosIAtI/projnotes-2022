c91c35fc977a54e3c131508e6e0155b32e7740bf
carlosIAtI <carlos_hdez.mtz@outlook.com>
2022-05-24 14:25:25 -0700

    implementado mvc

---
+++ server/controllers/odm.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+import  winston from './winston'
+
+class MoongoseODM {
+    constructor(url){
+        this.url = url;
+    }
+
+
+async connect(){
+    try {
+        //Agregandos la librerias globales de es6 a moongose
+        mongoose.Promise = global.Promise
+        winston.info(`🧩 Conectando a la base de datos ${this.url}`)
+        const connection = await mongoose.connect(this.url);
+        return connection;
+    } catch (error) {
+        winston.info(`❌ Error databases ❌ `)
+        winston.info(error)
+        
+    }
+}
+}
+
+export default MoongoseODM;
