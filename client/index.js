d316330611bc5491e6744d8dcb14adea03dc5809
carlosIAtI <carlos_hdez.mtz@outlook.com>
2022-05-25 07:06:06 -0700

    agregadno menu dropdwn

--- client/index.js
+++ client/index.js
@@ -8,6 +8,11 @@ import './styles/mystyle.css';
 document.addEventListener('DOMContentLoaded', () => {
   // Obteniendo la referencia a la barra de navegacion
   // lateral
+
   const sideNavs = document.querySelectorAll('.sidenav');
   M.Sidenav.init(sideNavs);
+
+  // Inicializando los Dropdown
+  const dropdown = document.querySelectorAll('.dropdown-trigger');
+  M.Dropdown.init(dropdown);
 });
