892126d0465dceb19adde6e7cfc81f9194a949a5
carlosIAtI <carlos_hdez.mtz@outlook.com>
2022-05-25 06:04:27 -0700

    s41 create Project-Model

---
+++ models/ProjectModel.js
@@ -0,0 +1,23 @@
+// ODM - Moongose 
+import mongoose from 'mongoose'
+
+const { Schema } = mongoose;
+
+const ProjectSchema = new Schema({
+    name: {
+        type: String,
+        require: true,
+    },
+    description: {
+        type: String,
+        require: true
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+}); // schema descripcion de los datos que va a contener el JSON "by PD. Rivalcova"
+
+
+// Generar el modelo a partir de un esquema => compilar el modelo-"crear una instancia a partir schema"
+export default mongoose.model('project',ProjectSchema);
