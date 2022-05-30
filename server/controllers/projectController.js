ff1d948fc462284747fa1c9ba3e9cbf1d292e14f
carlosIAtI <carlos_hdez.mtz@outlook.com>
2022-05-30 06:37:31 -0700

    s44 procesando ideas de projectos

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -8,7 +8,15 @@ const add = (req, res) => {
      res.render('projects/addProjectView', {})
 }
 
+// add projects
+const addProject = (req, res) => {
+    const { name, description } = req;
+    res.send(name,description)
+}
+
+
 export default {
     index,
-    add
+    add,
+    addProject,
 }       
--- server/routes/projectRouter.js
+++ server/routes/projectRouter.js
@@ -11,5 +11,7 @@ router.get(['/', '/index'], projectController.index);
 // send form to register project idea
 router.get('/add', projectController.add);
 
+// Procesa el formulario para agregar ideas de projectos
+router.post('/add ', projectController.addProject);
 
 export default router;
