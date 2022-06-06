61573cee503515b926c61dd618789178e125fb67
carlosIAtI <carlos_hdez.mtz@outlook.com>
2022-06-06 06:43:05 -0700

    s47 vallidator front

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -11,21 +11,37 @@ const add = (req, res) => {
 
 // add projects
 const addPost = (req, res) => {
-    const { errorData: error } = req;
-    if (error) {
+    const { errorData } = req;
+    // Crear view models para este action method
+    let project = {};
+    let errorModel = {};
+    if (errorData) {
       log.info('Se retorna objeto de error de validacion');
+      project = errorData.value;
+      // reduce 
+      errorModel = errorData.inner.reduce((prev, curr)=>{
+        ///// Mantener inmutables los elementos originales con ""const  newVal = pre;
+        const newVal = prev;
+        newVal[`${curr.path}Error`] = curr.message;
+        return newVal; 
+      }, {});
       // La validacion fallo
-      res.status(200).json(error);
-    } 
+      // res.status(200).json(errorModel);
+    }
+      log.info('Se retorna objeto Projecto valido');
       // Desestructurando la informacion
       // del formulario
-      const { validData: project } = req;
-      log.info('Se retorna objeto Projecto valido');
+      const { validData } = req;
       // Regresar un objeto con los datos
       // obtenidos del formulario
-      res.status(200).json(project);
-    
-}
+      project = validData;
+      // res.status(200).json(validData);
+      res.render('projects/addProjectView', {project, errorModel})
+      //res.status(200).json({project, errorModel});
+
+};
+
+
 
 
 export default {
